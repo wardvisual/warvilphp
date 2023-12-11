d64a2f3f2d5b5ca38a8429a5e5ee54f61d05bd31
root <wardvisual@gmail.com>
2023-12-11 23:51:05 +0800

    Warvilp - A PHP Framework (V1)

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -5,13 +5,22 @@ use \app\models\User;
 
 class Home extends Controller
 {
+    /**
+     * Constructor to set CORS headers.
+     */
     public function __construct()
     {
+        // Allow cross-origin requests from any origin
         header('Access-Control-Allow-Origin: *');
+        // Allow only specified methods for cross-origin requests
         header('Access-Control-Allow-Methods: POST, OPTIONS');
+        // Allow the Content-Type header for cross-origin requests
         header('Access-Control-Allow-Headers: Content-Type');
     }
 
+    /**
+     * Display the index page.
+     */
     public function index(): void
     {
         $payload = [
@@ -21,25 +30,33 @@ class Home extends Controller
         $this->view('home/index', $payload);
     }
 
+    /**
+     * Handle the user creation request.
+     */
     public function store(): void
     {
+        // Check if the request method is POST
         if (Request::isPostMethod()) {
             $userData = [
                 'name' => Request::input('name'),
             ];
 
+            // Attempt to create a new user
             $userCreated = User::create($userData);
 
+            // Check if user creation failed
             if (!$userCreated) {
                 Response::status(500)->json(['status' => 'error', 'message' => 'User creation failed']);
-                return;
             }
 
+            // Respond with success status if user created successfully
             Response::status(201)->json(['status' => 'success', 'message' => 'User created successfully']);
         } else {
+            // Respond with error status for invalid request method
             Response::status(405)->json(['status' => 'error', 'message' => 'Invalid request method']);
         }
 
+        // Exit the script after responding
         exit();
     }
 }
