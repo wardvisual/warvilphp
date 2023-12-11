fc30b2ff7ee2aebe4ee1831c8870d23f2c7d9fad
root <wardvisual@gmail.com>
2023-12-11 09:00:37 +0800

    up

--- app/core/App.php
+++ app/core/App.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 
 namespace app\core;
 
-class App {
+class App
+{
     protected $controller = 'Home';
     protected $method = 'index';
     protected $params = [];
@@ -13,7 +14,7 @@ class App {
 
         try {
             $url = $this->parseURL();
-    
+
             $this->setController($url);
             $this->setMethod($url);
             $this->setParams($url);
@@ -28,18 +29,18 @@ class App {
     private function setController(&$url)
     {
         if (!$url) $url = [$this->controller];
-    
+
         if (!file_exists('app/controllers/' . $url[0] . '.php'))
             throw new \Exception('Controller not found: ' . $url[0]);
-    
+
         $this->controller = $url[0];
         unset($url[0]);
-    
+
         require_once 'app/controllers/' . $this->controller . '.php';
-    
-        if (!class_exists($this->controller)) 
+
+        if (!class_exists($this->controller))
             throw new \Exception('Class does not exist: ' . $this->controller);
-    
+
         $this->controller = new $this->controller;
     }
 
@@ -54,7 +55,7 @@ class App {
         $this->method = $url[1];
         unset($url[1]);
     }
-    
+
     private function setParams($url)
     {
         $this->params = $url ? array_values($url) : [];
@@ -62,13 +63,12 @@ class App {
 
     public function parseURL()
     {
-        if(!isset($_GET['url'])) return;
-    
-        $url = rtrim($_GET['url'],'/');
-        $url = filter_var($url,FILTER_SANITIZE_URL);
-        $url = explode('/',$url);
+        if (!isset($_GET['url'])) return;
+
+        $url = rtrim($_GET['url'], '/');
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        $url = explode('/', $url);
 
         return $url;
     }
 }
-
