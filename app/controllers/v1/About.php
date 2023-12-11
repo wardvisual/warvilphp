29d151efe71a4030838cf4e6f0c26caa2393f16b
root <wardvisual@gmail.com>
2023-12-11 09:30:58 +0800

    up

---
+++ app/controllers/v1/About.php
@@ -0,0 +1,14 @@
+<?php 
+
+use \app\core\Controller;
+
+class About extends Controller {
+    public function index()
+    {
+        echo 'about';
+    }
+
+    public function test() {
+        echo 'test';
+    }
+}
