465c4308d019950d405c6ac5992c0f428c2c16d9
wardvisual <wardvisual@gmail.com>
2023-12-23 20:47:03 +0800

    WarvilPHP Framework

--- cli/help.php
+++ cli/help.php
@@ -3,7 +3,7 @@
 function displayHelp()
 {
     echo "\033[1;33m\nWarvilPHP Framework\n\n\033[0m";
-    echo "\033[1;32mphp cli/migration.php Class:up\033[0m - create a migration\n";
+    echo "\033[1;32mphp cli/migration.php Class:up\033[0m - run a clean migration\n";
     echo "\033[1;32mphp cli/controller.php ClassName\033[0m - create a controller\n";
     echo "\n";
 }
