af2ceaa4e503ddc4d31a1eb54727af5d9ea8aec0
wardvisual <wardvisual@gmail.com>
2023-12-26 12:13:49 +0800

    chore: configure routing

--- app/core/App.php
+++ app/core/App.php
@@ -2,6 +2,10 @@
 
 namespace app\core;
 
+use app\core\{Router, RouterApi};
+
+use app\controllers\{HomeController};
+
 class App
 {
     protected $controller = 'Home';
@@ -10,75 +14,21 @@ class App
 
     public function __construct()
     {
-        $this->controller = strtolower($this->controller);
+        $url = isset($_GET['url']) ? '/' . $_GET['url'] : '/';
+        $requestType = $_SERVER['REQUEST_METHOD'];
 
-        try {
-            $url = $this->parseURL();
-
-            $this->setController($url);
-            $this->setMethod($url);
-            $this->setParams($url);
-
-            call_user_func_array([$this->controller, $this->method], [$this->params]);
-        } catch (\Exception $e) {
-            echo '404 | Page not found.' . $e->getMessage();
-            // echo 'Page not found: <br/>next>> ' . $e->getMessage();
+        if (strpos($url, '/api') === 0) {
+            RouterApi::dispatch($url, $requestType);
+        } else {
+            Router::dispatch($url, $requestType);
         }
-    }
-
-    private function setController(&$url)
-    {
-        if (!$url) $url = [$this->controller];
-
-        if (!file_exists('app/controllers/' . $url[0] . '.php'))
-            throw new \Exception('Controller not found: ' . $url[0]);
-
-        $this->controller = $url[0];
-        unset($url[0]);
-
-        require_once 'app/controllers/' . $this->controller . '.php';
-
-        if (!class_exists($this->controller))
-            throw new \Exception('Class does not exist: ' . $this->controller);
-
-        $this->controller = new $this->controller;
-    }
-
-
-    private function setMethod(&$url)
-    {
-        if (!isset($url[1])) return;
-
-        if (!method_exists($this->controller, $url[1]))
-            throw new \Exception('Method not found: ' . $url[1]);
-
-        $this->method = $url[1];
-        unset($url[1]);
-    }
-
-    private function setParams($url)
-    {
-        $this->params = $url ? array_values($url) : [];
-    }
-
-    private function parseURL()
-    {
-        if (!isset($_GET['url'])) return;
-
-        $url = rtrim($_GET['url'], '/');
-        $url = filter_var($url, FILTER_SANITIZE_URL);
-        $url = explode('/', $url);
-
-        return $url;
     }
 
     public static function isProduction()
     {
         if ($_SERVER['HTTP_HOST'] == 'localhost') {
-            // echo "Development environment";
             return false;
         } else {
-            // echo "Production environment";
             return true;
         }
     }
