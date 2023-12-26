805c8a32e4c0b6b343176fc3cc7831bddb77d3bf
wardvisual <wardvisual@gmail.com>
2023-12-26 12:15:58 +0800

    feat: add validation

--- app/core/Request.php
+++ app/core/Request.php
@@ -38,19 +38,66 @@ class Request
         return $inputs[$item] ?? '';
     }
 
-    public static function file($item)
-    {
-        if (isset($_FILES[$item])) {
-            return $_FILES[$item];
-        }
-
-        return '';
-    }
-
     public static function body()
     {
         $body = file_get_contents('php://input');
         $body = json_decode($body, true);
         return $body;
+    }
+
+
+    public static function validate($rules)
+    {
+        $validatedData = [];
+        $errors = [];
+
+        foreach ($rules as $field => $rule) {
+            $value = self::input($field);
+            $validatedData[$field] = $value;
+
+            // Split the rules by '|'
+            $ruleParts = explode('|', $rule);
+
+            foreach ($ruleParts as $rulePart) {
+                // Check if the rule has parameters, e.g. 'max:255'
+                if (strpos($rulePart, ':') !== false) {
+                    list($ruleName, $ruleParameter) = explode(':', $rulePart);
+
+                    if ($ruleName === 'max' && strlen($value) > $ruleParameter) {
+                        $errors[$field][] = 'The ' . $field . ' may not be greater than ' . $ruleParameter . ' characters.';
+                    }
+
+                    if ($ruleName === 'min' && strlen($value) < $ruleParameter) {
+                        $errors[$field][] = 'The ' . $field . ' must be at least ' . $ruleParameter . ' characters.';
+                    }
+
+                    if ($ruleName === 'pattern' && !preg_match($ruleParameter, $value)) {
+                        $errors[$field][] = 'The ' . $field . ' format is invalid.';
+                    }
+                } else {
+                    if ($rulePart === 'required' && empty($value)) {
+                        $errors[$field][] = 'The ' . $field . ' field is required.';
+                    }
+
+                    if ($rulePart === 'email' && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                        $errors[$field][] = 'The ' . $field . ' must be a valid email address.';
+                    }
+
+                    if ($rulePart === 'digits' && !ctype_digit($value)) {
+                        $errors[$field][] = 'The ' . $field . ' must be a number.';
+                    }
+
+                    if ($rulePart === 'alpha' && !ctype_alpha(str_replace(' ', '', $value))) {
+                        $errors[$field][] = 'The ' . $field . ' may only contain letters.';
+                    }
+                }
+            }
+        }
+
+        if (!empty($errors)) {
+            throw new \Exception(json_encode($errors));
+        }
+
+        return $validatedData;
     }
 }
